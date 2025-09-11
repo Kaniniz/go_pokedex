87688f0b97627ebdaa19f3a97833c65d3d0b20e7
Kaniniz <majt1211@gmail.com>
2025-09-11 19:25:47 +0200

    REPL section works, two commands help and exit

---
+++ cli_commands.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+	for _, cmd := range allCommands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -3,18 +3,68 @@ package main
 import (
 	"fmt"
 	"strings"
+	"bufio"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	response := bufio.NewScanner(os.Stdin)
+	for true {
+		fmt.Print("Pokedex > ")
+		response.Scan()
+		cleanText := cleanInput(response.Text())
+		if err := response.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+
+		//First word in the user input is the command
+		input := cleanText[0]
+		command, exists := allCommands[input] 
+		if exists == false {
+			fmt.Println("Unknown command:", input)
+			continue
+		}
+
+		 err := command.callback()
+		 if err != nil {
+			fmt.Println("Error executing command:", err)
+		}
+
+	}
 }
+
+
 
 func cleanInput(text string) []string {
 	text, _ = strings.CutPrefix(text, " ")
 	text, _ = strings.CutSuffix(text, " ")
 	text = strings.ToLower(text)
-
 	words := strings.Split(text, " ")
-	
 	return words
 }
+
+type cliCommands struct {
+	name 		string
+	description	string
+	callback	func() error
+}
+
+//Declare the allCommands variable but assign the variables later to prevet an initialization cycle
+var allCommands = map[string]cliCommands{}
+
+func init() {
+	allCommands = map[string]cliCommands{
+		"help": {
+			name: 			"help",
+			description: 	"Lists all commands",
+			callback:		commandHelp,
+		},
+		"exit": {
+			name: 			"exit",
+			description:	"Exit the Pokedex",
+			callback: 		commandExit, 
+		},
+		
+	}
+}
+
